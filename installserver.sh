3d195fcebe6a93ac9fafabff0475977d2daa306d
Kris Manohar <justkrismanohar@gmail.com>
2016-11-22 18:52:56 -0400

    fixed forever ghost

--- installserver.sh
+++ installserver.sh
@@ -8,9 +8,10 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install nodejs
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo apt-get -y install npm
-sudo npm install express -g
-sudo npm install azure-sb -g
+sudo npm install express 
+sudo npm install azure-sb
 sudo npm install forever -g
 sudo forever start sb-single.js
+sudo forever list
 
 
