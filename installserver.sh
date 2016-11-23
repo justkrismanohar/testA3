d0edd27026f6d4bc1c3681fc72c3b5e46cbefc93
Kris Manohar <justkrismanohar@gmail.com>
2016-11-22 20:07:47 -0400

    cd in .sh

--- installserver.sh
+++ installserver.sh
@@ -2,7 +2,7 @@
 workserver_path=/srv/workserver
 sudo mkdir $workserver_path
 sudo cp sb-single.js $workserver_path
-
+sudo cd $workserver_path
 sudo apt-get update
 sudo apt-get -y install build-essential
 sudo apt-get -y install nodejs
