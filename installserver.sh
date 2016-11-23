ea685542e9485e4626dd48865add771096a03068
Kris Manohar <justkrismanohar@gmail.com>
2016-11-23 08:48:18 -0400

    /home/admin806003586

--- installserver.sh
+++ installserver.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 workserver_path=$HOME/workserver
+sudo echo $workserver_path
 sudo mkdir $workserver_path
 sudo cp sb-single.js $workserver_path
 
-sudo apt-get update
-sudo apt-get -y install build-essential
-sudo apt-get -y install nodejs
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-sudo apt-get -y install npm
+#sudo apt-get update
+#sudo apt-get -y install build-essential
+#sudo apt-get -y install nodejs
+#sudo ln -s /usr/bin/nodejs /usr/bin/node
+#sudo apt-get -y install npm
 
-sudo cd $workserver_path
-sudo npm install express 
-sudo npm install azure-sb
-sudo npm install forever -g
-sudo forever start sb-single.js
-sudo forever list
+#sudo cd $workserver_path
+#sudo npm install express 
+#sudo npm install azure-sb
+#sudo npm install forever -g
+#sudo forever start sb-single.js
+#sudo forever list
 
 
